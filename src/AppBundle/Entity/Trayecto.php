6fe0b5ade4b08b650a196bca1916b7cbf7f74df6
Ezequiel <toledano_87@hotmail.com>
2016-09-04 10:24:03 +0000

    Añadido funcion __toString y __construct a tabla Trayecto

--- src/AppBundle/Entity/Trayecto.php
+++ src/AppBundle/Entity/Trayecto.php
@@ -30,11 +30,11 @@ class Trayecto {
     /**
      * @ORM\Column(type="date")
      */
-    protected $fechaPublicacion;
+    protected $fechaDeViaje;
     /**
      * @ORM\Column(type="time")
      */
-    protected $hora;
+    protected $horaDeViaje;
     /**
      * @ORM\Column(type="float")
      */
@@ -52,6 +52,17 @@ class Trayecto {
      * @ORM\JoinColumn(name="persona_id", referencedColumnName="id")
     */
     protected $conductor;
+
+    public function __construct()
+    {
+        $this->fechaDeViaje= New\DateTime();
+        $this->horaDeViaje= New\DateTime();
+    }
+    
+    public function __toString()
+    {
+        return "Viaje de". $this->getOrigen(). "a". $this->getDestino();
+    }
 
     /**
      * Get id
@@ -133,49 +144,49 @@ class Trayecto {
     }
 
     /**
-     * Set fechaPublicacion
+     * Set fechaDeViaje
      *
-     * @param \DateTime $fechaPublicacion
+     * @param \DateTime $fechaDeViaje
      * @return Trayecto
      */
-    public function setFechaPublicacion($fechaPublicacion)
+    public function setFechaDeViaje($fechaDeViaje)
     {
-        $this->fechaPublicacion = $fechaPublicacion;
+        $this->fechaDeViaje = $fechaDeViaje;
 
         return $this;
     }
 
     /**
-     * Get fechaPublicacion
+     * Get fechaDeViaje
      *
      * @return \DateTime 
      */
-    public function getFechaPublicacion()
+    public function getFechaDeViaje()
     {
-        return $this->fechaPublicacion;
+        return $this->fechaDeViaje;
     }
 
     /**
-     * Set hora
+     * Set horaDeViaje
      *
-     * @param \DateTime $hora
+     * @param \DateTime $horaDeViaje
      * @return Trayecto
      */
-    public function setHora($hora)
+    public function setHoraDeViaje($horaDeViaje)
     {
-        $this->hora = $hora;
+        $this->horaDeViaje = $horaDeViaje;
 
         return $this;
     }
 
     /**
-     * Get hora
+     * Get horaDeViaje
      *
      * @return \DateTime 
      */
-    public function getHora()
+    public function getHoraDeViaje()
     {
-        return $this->hora;
+        return $this->horaDeViaje;
     }
 
     /**
