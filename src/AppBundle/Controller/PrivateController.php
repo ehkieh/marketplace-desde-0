801480f5734d37db81c3d15b30664ad6d22dfcc3
Ezequiel <toledano_87@hotmail.com>
2016-09-05 09:20:47 +0000

    Completamos la accion nuevoTrayecto

--- src/AppBundle/Controller/PrivateController.php
+++ src/AppBundle/Controller/PrivateController.php
@@ -1,28 +1,26 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 class PrivateController extends Controller
 {
     /**
-     * @Route("/nuevo_Trayecto", name="private_nuevoTrayecto")
-    */
-     public function nuevoTrayectoAction(Request $request)
-     {
-        die("Pendiente de Hacer: Nuevo Trayecto");
-     }
-     
-    /**
-     * @Route("/publicar_Trayecto", name="private_publicarTrayecto")
-    */
-    public function publicarTrayectoAction(Request $request)
-    
+     * @Route("/nuevoTrayecto", name="private_nuevoTrayecto")
+     */
+    public function nuevoTrayectoAction(Request $request)
     {
-    die("Pendiente de Hacer: Publicar Trayecto");
+       
+        return $this->render('nuevoTrayecto/index.html.twig');
     }
+    
+    /**
+     * @Route("/publicarTrayecto", name="private_publicarTrayecto")
+     */ 
+    public function publicarTrayectoAction(Request $request)
+    {
         
+        die("Pendiente de hacer: Publicar Trayecto");
+       
+    }
 }
