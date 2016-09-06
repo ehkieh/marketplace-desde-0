4094f1ebb2cf93f66b74d0ca0975258bad574e29
Ezequiel <toledano_87@hotmail.com>
2016-09-06 14:45:24 +0000

    7.5. - Incorporamos campos de creación y actualización a Trayecto

--- src/AppBundle/Entity/Trayecto.php
+++ src/AppBundle/Entity/Trayecto.php
@@ -3,12 +3,14 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="trayecto")
 */
 class Trayecto {
+use ORMBehaviors\Timestampable\Timestampable;
     /**
     * @ORM\Column(type="integer")
     * @ORM\Id
