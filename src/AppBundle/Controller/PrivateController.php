f3cdd6100fd58156d7cc9788e0b293500a0184e2
Ezequiel <toledano_87@hotmail.com>
2016-09-06 09:32:18 +0000

    Completamos la funcion publicar Trayecto

--- src/AppBundle/Controller/PrivateController.php
+++ src/AppBundle/Controller/PrivateController.php
@@ -1,5 +1,7 @@
 <?php
 namespace AppBundle\Controller;
+
+use AppBundle\Entity\Trayecto;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,8 +21,38 @@ class PrivateController extends Controller
      */ 
     public function publicarTrayectoAction(Request $request)
     {
+        //Creamos la entidad Trayecto
+        $nuevoTrayecto= new Trayecto();
         
-        die("Pendiente de hacer: Publicar Trayecto");
-       
+        //Asignamos los valores recogidos por Request del form
+        $nuevoTrayecto->setOrigen(($request->get('origen')));
+        $nuevoTrayecto->setDestino(($request->get('destino')));
+        $nuevoTrayecto->setCalle(($request->get('calle')));
+        
+        //Creamos el objeto \DateTime y lo almacenamos en $fechaDateTime
+        $fechaDateTime = new \DateTime(($request->get('fechaDeViaje')));
+        $nuevoTrayecto->setFechaDeViaje($fechaDateTime);
+        
+        //Creamos el objeto \DateTime y lo almacenamos en $horaDateTime
+        $horaDateTime = new \DateTime(($request->get('horaDeViaje')));
+        $nuevoTrayecto->setHoraDeViaje($horaDateTime);
+        //Asignamos los valores recogidos por Request del form
+        $nuevoTrayecto->setPrecio(($request->get('precio')));
+        $nuevoTrayecto->setDescripcion(($request->get('descripcion')));
+        $nuevoTrayecto->setPlazas(($request->get('plazas')));
+        //Utilizamos el valor de User y lo almacenamos en la variable $usuarioLogueado
+        $usuarioLogueado = $this->getUser();
+        $nuevoTrayecto->setConductor($usuarioLogueado);
+        
+        //Hacemos un persist y un flush de los datos que hemos ido almacenando
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($nuevoTrayecto);
+        $entityManager->flush();
+        
+        //Explicacion para entenderlo mejor
+        //$urlForRedirect = $this->generateUrl("...");
+        //return $this->redirect($urlForRedirect);
+        return $this->redirect($this->generateUrl('public_home'));
+        
     }
 }
